4c2254581f9b8763f66cc290cf092f1ea9e9abee
j-belec <jeronimobelec@hotmail.com>
2024-04-03 23:46:16 -0300

    changing to sequelize and some comments

--- src/controllers/appController.js
+++ src/controllers/appController.js
@@ -1,19 +1,18 @@
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+// const Phrase = require("../models/Phrase");
 
 module.exports = {
   home: async function (req, res) {
     res.render("home");
   },
-  lang: async function (req, res) {
-    const language = req.params.lang;
+  // lang: async function (req, res) {
+  //   const language = req.params.lang;
 
-    const phrasesArr = await prisma.phrase.findMany({
-      where: { lang: language },
-    });
-    const id = Math.round(Math.random() * (phrasesArr.length - 1));
-    const phraseArr = phrasesArr[id].content.split(" ");
+  //   const phrasesArr = await Phrase.findAll({
+  //     where: { lang: language },
+  //   });
+  //   const id = Math.round(Math.random() * (phrasesArr.length - 1));
+  //   const phraseContent = phrasesArr[id].dataValues.content.split(" ");
 
-    res.render("game", { phrase: phraseArr, lang: language });
-  },
+  //   res.render("game", { phrase: phraseContent, lang: language });
+  // },
 };
--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const appController = require("../controllers/appController");
 
 router.get("/", appController.home);
-router.get("/language/:lang", appController.lang);
+// router.get("/language/:lang", appController.lang);
 
 module.exports = router;
